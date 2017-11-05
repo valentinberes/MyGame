de8bf2ae1f6b7e6976bf72dde431a12443a73ad9
Valentin Beres <valentinberes@outlook.com>
2017-11-05 17:58:03 -0500

    Finished everything except collision

--- app/src/main/java/com/valen/mygame/MainActivity.java
+++ app/src/main/java/com/valen/mygame/MainActivity.java
@@ -1,13 +1,41 @@
 package com.valen.mygame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by valen on 11/05/2017.
+ * Project: MyGame
+ */
 
 public class MainActivity extends AppCompatActivity {
+    // fields
+    private GameView myGameView;
 
+    // methods
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        myGameView = new GameView(this);
+        setContentView(myGameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        myGameView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to reallocate them.
+        myGameView.onResume();
     }
 }
